cb39f69fe0e65ec0d442dc5f81baee4ac38a5142
Hieu PC <hieupc@Mac-Pro-M1-256GB-cua-Hieu-PC.local>
2026-05-09 10:27:45 +0700

    Configure Supabase project

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.MANDARIN_CAPTURE_CONFIG = {
-  supabaseUrl: "",
-  supabaseAnonKey: ""
+  supabaseUrl: "https://loxbneuilhdkneuuouhr.supabase.co",
+  supabaseAnonKey: "sb_publishable_IMV8xXpBvlk4NgwS7HE4ig_01oZvYO6"
 };
